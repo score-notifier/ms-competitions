e0e8d52fd5e95751f21f943d6a9d902e6da76751
bjdose <brayanjsalcedo@gmail.com>
2024-07-12 10:19:55 -0600

    chore: Refactor main.ts for readability and consistency

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { envs } from 'src/config';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {Logger, ValidationPipe} from '@nestjs/common';
+import {MicroserviceOptions, Transport} from '@nestjs/microservices';
+import {envs} from 'src/config';
 
 async function bootstrap() {
-  const logger = new Logger('CompetitionsService');
+    const logger = new Logger('CompetitionsService');
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.NATS,
-      options: {
-        servers: envs.natsServers,
-      },
-    },
-  );
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+        AppModule,
+        {
+            transport: Transport.NATS,
+            options: {
+                servers: envs.natsServers,
+            },
+        },
+    );
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
 
-  await app.listen();
+    await app.listen();
 
-  logger.log(`Competitions microservice running on port ${envs.port}!`);
+    logger.log(`Competitions microservice running on port ${envs.port}`);
 }
 
 bootstrap();
